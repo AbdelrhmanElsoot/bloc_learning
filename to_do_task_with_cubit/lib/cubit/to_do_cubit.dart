276b7246df7e6979d83b42a6e89d45e401316e28
Elsoot <Abdelrhman.A.Elsoot@gmail.com>
2025-08-12 09:20:01 +0300

    Update (TaskModel , addTask Fn)

--- to_do_task_with_cubit/lib/cubit/to_do_cubit.dart
+++ to_do_task_with_cubit/lib/cubit/to_do_cubit.dart
@@ -2,26 +2,28 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:to_do_task_with_cubit/models/task_model.dart';
+import 'package:uuid/uuid.dart';
 
 part 'to_do_state.dart';
 
 class ToDoCubit extends Cubit<ToDoState> {
   ToDoCubit() : super(ToDoInit());
 
-  void addTask({required TaskModel taskModel}) {
+  void addTask({required String title}) {
     // Copy the old task list and add the new task
     // We use ... to copy all items
+    TaskModel taskModel = TaskModel(Uuid().v4(), title, false);
     emit(ToDoUpdate([...state.task, taskModel]));
   }
 
-  void removeTask({required int id}) {
+  void removeTask({required String id}) {
     // Copy old tasks but skip the one with this id
     final updateTasks = state.task.where((task) => task.id != id).toList();
     // Emit new state with updated task list
     emit(ToDoUpdate(updateTasks));
   }
 
-  void toggleTask({required int id}) {
+  void toggleTask({required String id}) {
     // Make a new list of tasks
     final updateTasks = state.task.map((task) {
       if (task.id == id) {
